5048d83b3cd030466b79d48edb73ad0fc88ed991
Ashlyn Aiu <ashlyn.aiu@gmail.com>
2016-09-21 12:17:43 -0700

    implement top slider

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,6 +2,18 @@
 
 angular.module('DF16Wealth')
 	.controller('MainCtrl', function ($scope, clients) {
+		$scope.showClientList = false;
+		$scope.showWave = true;
+		$scope.showFiler = false;
+
+		$scope.toogleClientList = function() {
+			if ($scope.showClientList === false) {
+				$scope.showClientList = true;
+			}
+			else {
+				$scope.showClientList = false;
+			}
+		};
 
 		clients.items().success(function(data){
 				$scope.clients = data;
