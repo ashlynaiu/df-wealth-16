2a1fff43d4ddc3cfabc61205121e0f3db9e42069
Ashlyn Aiu <ashlyn.aiu@gmail.com>
2016-09-26 16:44:17 -0700

    Enable wwwhisper

--- web.js
+++ web.js
@@ -3,6 +3,15 @@ var express = require('express');
 var app = express();
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 
+var wwwhisper = require('connect-wwwhisper');
+// app holds a reference to express or connect framework, it
+// may be named differently in your source file.
+app.use(wwwhisper());
+
+// Alternatively, if you don't want wwwhisper to insert
+// a logout iframe into HTML responses use.
+app.use(wwwhisper(false));
+
 app.get('/*', function(req, res){
   res.sendFile(__dirname + '/dist/index.html');
 });
